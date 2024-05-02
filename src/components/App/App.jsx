2a29a43003fe61f8b6b0bac5d9e351b4d686cfc0
OksanaBakhaieva <obahaeva@gmail.com>
2024-05-02 18:37:55 +0100

    Creating of structure, Layout, Navigation, App, Routes

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+
+import { Route, Routes } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import HomePage from "../../pages/HomePage/HomePage";
+import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
+import MovieCast from "../MovieCast/MovieCast";
+import MovieReviews from "../MovieReviews/MovieReviews"; 
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+  );
+}
---
+++ src/movies-api.js
@@ -0,0 +1,15 @@
+// API_KEY=d0a6e50b86282614d50c640265680d21
+// Trending movies - список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// https://api.themoviedb.org/3/trending/movie/{time_window}
+
+// Search movie - пошук фільму за ключовим словом на сторінці фільмів.
+// https://api.themoviedb.org/3/search/movie
+
+// Movie details - запит повної інформації про фільм для сторінки кінофільму.
+// https://api.themoviedb.org/3/movie/{movie_id}
+
+// Movie credits - запит інформації про акторський склад для сторінки кінофільму.
+// https://api.themoviedb.org/3/movie/{movie_id}/credits
+
+// Movie reviews - запит оглядів для сторінки кінофільму.
+// https://api.themoviedb.org/3/movie/{movie_id}/reviews
